ebe30e26f4077f705b50a982f737c12d7a3a0468
Kan <xkk19990810@gmail.com>
2023-02-20 23:39:51 +0800

    2023/2/20

--- 链表/MtLinkedList.py
+++ 链表/MtLinkedList.py
@@ -1,7 +1,7 @@
 class Node(object):
     def __init__(self, x = 0):
         self.val = x
-        self.next = Node
+        self.next = None
 
 class MyLinkedList(object):
 
@@ -21,7 +21,43 @@ class MyLinkedList(object):
     def addAtHead(self, val):
         new_node = Node(val)
         new_node.next = self.head.next
-        self.head.next = new.node
+        self.head.next = new_node
         self.size += 1
 
-    def add
+    def addAtTail(self, val):
+        new_node = Node(val)
+        cur = self.head
+        while(cur.next):
+            cur = cur.next
+        cur.next = new_node
+        self.size += 1
+    def addAtIndex(self, index, val):
+        if index < 0:
+            self.addAtHead(val)
+            return
+        elif index == self.size:
+            self.addAtTail(val)
+            return
+        elif index > self.size:
+            return
+
+        node = Node(val)
+        pre = self.head
+        while(index):
+            pre = pre.next
+            index -= 1
+        node.next = pre.next
+        pre.next = node
+        self.size += 1
+
+    def deleteAtIndex(self, index):
+        if index < 0 or index >= self.size:
+            return
+        pre = self.head
+        while(index):
+            pre = pre.next
+            index -= 1
+        pre.next = pre.next.next
+        self.size -= 1
+
+
