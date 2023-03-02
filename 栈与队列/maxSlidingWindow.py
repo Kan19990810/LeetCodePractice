2f3b7cea8a72257ba80d62630fc941036af298a2
Kan19990810 <xkk19990810@gmail.com>
2023-03-02 11:22:31 +0800

    2023/3/2

--- 栈与队列/maxSlidingWindow.py
+++ 栈与队列/maxSlidingWindow.py
@@ -7,5 +7,32 @@
 链接：https://leetcode.cn/problems/sliding-window-maximum
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 """
+from collections import deque
+
+class MyQueue:
+    def __init__(self):
+        self.queue = deque()
+
+    def pop(self, value):
+        if self.queue and value == self.queue[0]:
+            self.queue.popleft()
+
+    def push(self, value):
+        while self.queue and value >self.queue[-1]:
+            self.queue.pop()
+        self.queue.append(value)
+
+    def front(self):
+        return self.queue[0]
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        que = MyQueue()
+        result = []
+        for i in range(k):
+            que.push(nums[i])
+        result.append(que.front())
+        for i in range(k, len(nums)):
+            que.pop(nums[i - k])
+            que.push(nums[i])
+            result.append(que.front())
+        return result
