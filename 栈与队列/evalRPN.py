9c8b58ecd04f3c655bda1b271fd4a38058b63ce5
Kan19990810 <xkk19990810@gmail.com>
2023-03-02 10:37:19 +0800

    2023/3/2

---
+++ 栈与队列/evalRPN.py
@@ -0,0 +1,44 @@
+"""
+给你一个字符串数组 tokens ，表示一个根据 逆波兰表示法 表示的算术表达式。
+
+请你计算该表达式。返回一个表示表达式值的整数。
+
+注意：
+
+有效的算符为 '+'、'-'、'*' 和 '/' 。
+每个操作数（运算对象）都可以是一个整数或者另一个表达式。
+两个整数之间的除法总是 向零截断 。
+表达式中不含除零运算。
+输入是一个根据逆波兰表示法表示的算术表达式。
+答案及所有中间计算结果可以用 32 位 整数表示。
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/evaluate-reverse-polish-notation
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+from operator import add, sub, mul
+
+class Solution:
+    op_map = {'+': add, '-': sub, '*':mul, '/': lambda x, y: int(x / y)}
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for token in tokens:
+            if token not in {'+', '-', '*', '/'}:
+                stack.append(int(token))
+            else:
+                op2 = stack.pop()
+                op1 = stack.pop()
+                stack.append(self.op_map[token](op1, op2))
+        return stack.pop()
+
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for item in tokens:
+            if item not in {"+", "-", "*", "/"}:
+                stack.append(item)
+            else:
+                first_num, second_num = stack.pop(), stack.pop()
+                stack.append(
+                    int(eval(f'{second_num}{item}{first_num}'))
+                )
+        return int(stack.pop())
