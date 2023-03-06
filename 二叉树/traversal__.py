176d7074e164da86336f5c1a7711de5b17d02a15
Kan19990810 <xkk19990810@gmail.com>
2023-03-06 19:30:35 +0800

    2023/3/6

--- 二叉树/traversal__.py
+++ 二叉树/traversal__.py
@@ -5,3 +5,62 @@ class TreeNode:
         self.right =None
 class Solution:
     def preorderTraversal(self, root: TreeNode) -> List[int]:
+        result = []
+        st = []
+        if root:
+            st.append(root)
+        while st:
+            node = st.pop()
+            if node != None:
+                if node.right:
+                    st.append(node.right)
+                if node.left:
+                    st.append(node.left)
+                st.append(node)
+                st.append(None)
+            else:
+                node = st.pop()
+                result.append(node.val)
+        return result
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        result = []
+        st = []
+        if root:
+            st.append(root)
+        while st:
+            node = st.pop()
+            if node != None:
+                if node.right:
+                    st.append(node.right)
+
+                    st.append(node)
+                    st.append(None)
+
+                if node.left:
+                    st.append(node.left)
+            else:
+                node = st.pop()
+                result.append(node.val)
+        return result
+
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        result = []
+        st = []
+        if root:
+            st.append(root)
+        while st:
+            node = st.pop()
+            if node != None:
+                st.append(node)
+                st.append(None)
+                if node.right:
+                    st.append(node.right)
+                if node.left:
+                    st.append(node.left)
+            else:
+                node = st.pop()
+                result.append(node.val)
+        return result
