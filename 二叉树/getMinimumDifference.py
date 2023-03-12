38c04a6c42e0a04221cf23f46c611fe374f5c986
Kan <xkk19990810@gmail.com>
2023-03-12 12:19:13 +0800

    2023/3/12

--- 二叉树/getMinimumDifference.py
+++ 二叉树/getMinimumDifference.py
@@ -8,4 +8,34 @@ class TreeNode:
         self.right = right
 class Solution:
     def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
-        
+        res = []
+        r = float("inf")
+        def buildaList(root):
+            if not root: return None
+            if root.left: buildaList(root.left)
+            res.append(root.val)
+            if root.right: buildaList(root.right)
+            return res
+
+        buildaList(root)
+        for i in range(len(res) - 1):
+            r = min(abs(res[i] - res[i + 1]), r)
+        return r
+
+class Solution:
+    def getMinimumDifference(self, root: TreeNode) -> int:
+        stack = []
+        cur = root
+        pre = None
+        result = float("inf")
+        while cur or stack:
+            if cur:
+                stack.append(cur)
+                cur = cur.left
+            else:
+                cur = stack.pop()
+                if pre:
+                    result = min(result, abs(cur.val - pre.val))
+                pre = cur
+                cur = cur.right
+        return result
