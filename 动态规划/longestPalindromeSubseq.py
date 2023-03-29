6e83410671dcb943f530d71465215657f023b670
Kan19990810 <xkk19990810@gmail.com>
2023-03-29 10:54:02 +0800

    2023/3/29

---
+++ 动态规划/longestPalindromeSubseq.py
@@ -0,0 +1,19 @@
+"""
+给定一个字符串 s ，找到其中最长的回文子序列，并返回该序列的长度。可以假设 s 的最大长度为 1000 。
+
+示例 1: 输入: "bbbab" 输出: 4 一个可能的最长回文子序列为 "bbbb"。
+
+示例 2: 输入:"cbbd" 输出: 2 一个可能的最长回文子序列为 "bb"。
+"""
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        dp = [[0] * len(s) for _ in range(len(s))]
+        for i in range(len(s)):
+            dp[i][i] = 1
+        for i in range(len(s) - 1, -1, -1):
+            for j in range(i + 1, len(s)):
+                if s[i] == s[j]:
+                    dp[i][j] = dp[i + 1][j - 1] + 2
+                else:
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
+        return dp[0][-1]
