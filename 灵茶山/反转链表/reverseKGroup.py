a775b5b99302d4e799819afc773c1defe7c6c45d
Kan19990810 <xkk19990810@gmail.com>
2023-04-24 20:23:58 +0800

    2023/4/24

---
+++ 灵茶山/反转链表/reverseKGroup.py
@@ -0,0 +1,10 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        dummy = ListNode(next=head)
+        pre = dummy
+        cur = head
+        n = len()
