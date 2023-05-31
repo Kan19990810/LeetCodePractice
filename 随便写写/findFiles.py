1f9bb637dcd6c6061efcb6aeacfa7ca04061e5ea
Kan19990810 <xkk19990810@gmail.com>
2023-05-31 20:18:47 +0800

    2023/5/31

---
+++ 随便写写/findFiles.py
@@ -0,0 +1,9 @@
+import glob
+import shutil
+
+if __name__ == '__main__':
+    shutil.move('/public/home/xiaok/xiaok/data/dev', '/public/home/xiaok/xiaok/data/sitw')
+    shutil.move('/public/home/xiaok/xiaok/data/evel', '/public/home/xiaok/xiaok/data/sitw')
+    shutil.move('/public/home/xiaok/xiaok/data/README.txt', '/public/home/xiaok/xiaok/data/sitw')
+
+
