46499ea70dafd52f0cff6df7be83d117245be40e
Kan19990810 <xkk19990810@gmail.com>
2023-07-10 14:38:28 +0800

    leetcode

---
+++ dayly/2023/7/threeSumClosest.py
@@ -0,0 +1,26 @@
+from math import inf
+
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        nums.sort()
+        diff = inf
+        res = 0
+        for i in range(n - 2):
+            left, right = i + 1, n - 1
+            s = 0
+            while left < right:
+                s = nums[i] + nums[left] + nums[right
+                                                ]
+                curD = abs(target - s)
+                if curD < diff:
+                    diff = curD
+                    res = s
+                if s > target:
+                    right -= 1
+                elif s < target:
+                    left += 1
+                else:
+                    return target
+        return res
